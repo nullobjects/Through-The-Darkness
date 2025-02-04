e7c4bfd510d8ec24885d7715a0ab371a00e26c10
Bobi Bogdanoski <bobibogdanoski002@gmail.com>
2025-02-04 21:04:07 +0100

    fix: many bug fixes and changes

--- Assets/PlayerDialog.cs
+++ Assets/PlayerDialog.cs
@@ -2,22 +2,34 @@ using System.Collections;
 using UnityEngine;
 using TMPro;
 using System.Collections.Generic;
+using UnityEngine.UI; // Needed for LayoutRebuilder
 
 public class PlayerDialog : MonoBehaviour {
-    GameObject dialogBox;
-    TMP_Text messageText;
-    CanvasGroup canvasGroup;
-    Queue<string> messageQueue = new Queue<string>();
-    bool isTyping = false;
+    public GameObject dialogBox;
+    public TMP_Text messageText;
+    public RectTransform messagePanel;
+    public CanvasGroup canvasGroup;
+    public Queue<string> messageQueue = new Queue<string>();
+    public bool isTyping = false;
 
     void Start() {
-        dialogBox = transform.Find("Canvas_DialogBox")?.gameObject;
+        if (dialogBox == null) {
+            dialogBox = transform.Find("Canvas_DialogBox")?.gameObject;
+        }
         if (dialogBox != null) {
-            Transform messageTextTransform = dialogBox.transform.Find("MessageText");
-            if (messageTextTransform != null) {
-                messageText = messageTextTransform.GetComponent<TMP_Text>();
-            }
+            messageText = dialogBox.transform.Find("MessageText")?.GetComponent<TMP_Text>();
+            messagePanel = dialogBox.transform.Find("MessagePanel")?.GetComponent<RectTransform>();
             canvasGroup = dialogBox.GetComponent<CanvasGroup>();
+            
+            if (messagePanel != null) {
+                messagePanel.sizeDelta = new Vector2(0,0);
+            }
+
+            if (messageText != null) {
+
+                ResizeBubble();
+            }
+
             if (canvasGroup != null) {
                 canvasGroup.alpha = 0;
                 canvasGroup.interactable = false;
@@ -39,12 +51,18 @@ public class PlayerDialog : MonoBehaviour {
 
     IEnumerator GenerateText(string text) {
         if (messageText != null) {
+            ResizeBubble();
+            messageText.text = "";
+
+            LayoutRebuilder.ForceRebuildLayoutImmediate(messageText.rectTransform);
+            yield return null;
+
             canvasGroup.alpha = 1;
             canvasGroup.blocksRaycasts = true;
 
-            messageText.text = "";
             foreach (char letter in text) {
                 messageText.text += letter;
+                ResizeBubble();
                 yield return new WaitForSeconds(0.05f);
             }
 
@@ -54,6 +72,9 @@ public class PlayerDialog : MonoBehaviour {
             canvasGroup.interactable = false;
             canvasGroup.blocksRaycasts = false;
 
+            messageText.text = "";
+            ResizeBubble();
+
             if (messageQueue.Count > 0) {
                 StartCoroutine(GenerateText(messageQueue.Dequeue()));
             } else {
@@ -61,4 +82,21 @@ public class PlayerDialog : MonoBehaviour {
             }
         }
     }
+
+    void ResizeBubble() {
+        if (messagePanel != null && messageText != null) {
+            LayoutRebuilder.ForceRebuildLayoutImmediate(messageText.rectTransform);
+
+            float newWidth = messageText.textBounds.size.x;
+            float newHeight = messageText.textBounds.size.y * 2f - 0.6f;
+
+            messagePanel.sizeDelta = new Vector2(-2.5f + newWidth, -0.4f + newHeight);
+
+            messagePanel.pivot = new Vector2(0f, 1f);
+
+            Vector2 currentPos = messagePanel.anchoredPosition;
+            messagePanel.anchoredPosition = new Vector2(currentPos.x, currentPos.y);
+        }
+    }
+
 }
